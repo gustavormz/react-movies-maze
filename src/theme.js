44fa9f1722c1d130214dc70d362db5ea44780203
dramirez1 <dramirez1@godaddy.com>
2021-12-24 00:12:38 -0600

    added theme

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#272643'
+    },
+    secondary: {
+      main: '#bae8e8'
+    }
+  }
+});
+
+export default theme;
