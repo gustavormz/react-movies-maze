8f7e76ac865034f71af1c7c0958d76613705146f
dramirez1 <dramirez1@godaddy.com>
2021-12-23 22:58:28 -0600

    added showDetails cases on reducer

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,10 @@ import React, { createContext, useReducer } from 'react';
 
 import {
   SET_SHOWS,
-  GET_SHOWS_BY_PAGINATION,
-  SET_PAGINATION
+  SET_PAGINATION,
+  SET_SHOW_ID,
+  SET_IS_DIALOG_OPEN,
+  SET_SHOW_DETAILS
 } from '../actionStatement/show';
 
 import utils from '../lib/utils';
@@ -12,14 +14,15 @@ export const StoreContext = createContext({});
 
 const initialState = {
   shows: [],
-  detailShow: {},
+  showDetails: {},
   pagination: {
     total: 0,
     page: 1,
     size: 12,
     count: 0
   },
-  showsToDisplay: []
+  showsToDisplay: [],
+  isDialogOpen: false
 };
 
 const reducer = (state, action) => {
@@ -48,6 +51,29 @@ const reducer = (state, action) => {
           page: action.payload.page
         },
         showsToDisplay: [...showsToDisplay]
+      };
+    case SET_IS_DIALOG_OPEN:
+      return {
+        ...state,
+        isDialogOpen: action.payload.isDialogOpen
+      };
+    case SET_SHOW_ID:
+      return {
+        ...state,
+        showDetails: {
+          id: action.payload.showId
+        },
+        isDialogOpen: true
+      };
+    case SET_SHOW_DETAILS:
+      const showDetails = utils.formatDataShow({ show: action.payload.showDetails });
+      return {
+        ...state,
+        showDetails: {
+          id: state.showDetails.id,
+          ...showDetails
+        },
+        isDialogOpen: true
       };
     default:
       throw new Error ('Define action type');
